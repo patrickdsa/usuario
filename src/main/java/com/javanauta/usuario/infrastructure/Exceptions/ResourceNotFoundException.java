ad732f21efdcf4b8863f0ba836d12a1821b30254
Patrick de Sá <patrickdesainfo@gmail.com>
2026-04-29 11:04:32 +0100

    adicionando métodos de login/delete/get

---
+++ src/main/java/com/javanauta/usuario/infrastructure/Exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.javanauta.usuario.infrastructure.Exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable throwable) {
+        super(message);
+    }
+
+}
